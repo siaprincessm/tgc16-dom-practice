f810c9a017c678c8214c7ab14f25d6b38eeeddca
siaprincessm <siaprincessm@gmail.com>
2023-06-19 17:00:19 +0000

    answer q1 and set up the testing environment

--- q1_basics_of_selecting/script.js
+++ q1_basics_of_selecting/script.js
@@ -1,5 +1,10 @@
 // PLACE YOUR CODE HERE
+let header = document.querySelector('h1');
 
+
+let important = document.querySelector('span');
+
+let todo = document.querySelector('li');
 
 
 // LEAVE THE BELOW ALONE! DON'T TOUCH
