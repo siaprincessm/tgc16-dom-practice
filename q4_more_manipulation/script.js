09356b35c221140298830d96efaa77ed3f246f87
siaprincessm <siaprincessm@gmail.com>
2023-08-30 08:04:56 +0000

    answere q4 no 1 and 2 only

---
+++ q4_more_manipulation/script.js
@@ -0,0 +1,10 @@
+//Select the first `<h1>` element in the list. Change it so that it now displays in green color.
+document.querySelector('h1').style.background = "green";
+
+//Change the text of the first `<li class="finished">` to the string "Repay credit card debt"
+document.querySelector(".finished").innerText = "Repay credit card debt"
+
+//Select the element `<li id="urgent">`, and give it a black coloured border that has a width of '2px'. Hint: Check the `border-color`
+//and the `border-width` css properties on a web page.
+
+
