89a734f5004a8d0d076d992c0ce5cacccc24bd1b
siaprincessm <siaprincessm@gmail.com>
2023-08-30 18:11:20 +0000

    answere q4 nos 3 4 5 and 6

--- q4_more_manipulation/script.js
+++ q4_more_manipulation/script.js
@@ -6,5 +6,20 @@ document.querySelector(".finished").innerText = "Repay credit card debt"
 
 //Select the element `<li id="urgent">`, and give it a black coloured border that has a width of '2px'. Hint: Check the `border-color`
 //and the `border-width` css properties on a web page.
+document.querySelector('#urgent').style.border = "2px solid black";
 
+//Change the first element with the class `emphasis` such that it is underlined 
+//(check out the text decoration css style online)
+document.querySelector('.emphasis').style.textDecoration = "underline";
 
+//Select first element with `<p class='emphasis'>` and change its background color to orange,
+ //its font size to "32px" and its line height to be "24px".
+ document.querySelector('p.emphasis').style.background = "orange";
+ document.querySelector('p.emphasis').style.fontSize = "32px";
+ document.querySelector('p.emphasis').style.lineHeight= "24px";
+
+ //Select the `<span class="todo">` which is nested inside the `<li id="urgent">` and give
+ //it a background color of 'yellow'
+ document.querySelector('#top').style.background = "yellow";
+
+\
