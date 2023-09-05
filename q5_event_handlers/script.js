b3f433593b2db977ff0d218e36124486702ae333
siaprincessm <siaprincessm@gmail.com>
2023-09-05 06:52:15 +0000

    answered q5

--- q5_event_handlers/script.js
+++ q5_event_handlers/script.js
@@ -1,3 +1,6 @@
 // PLACE YOUR CODE HERE
-
+let changeColorButton = document.querySelector('#changeColor');
+    changeColorButton.addEventListener('click', function(){
+        changeColorButton.style.backgroundColor = 'yellow';
+    })
 
